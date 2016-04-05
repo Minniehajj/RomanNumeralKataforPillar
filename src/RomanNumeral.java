2f0584ac95b560c2c0e5d6a52e473135ed27668c
minniehajj <mhoq@mapsysinc.com>
2016-04-05 15:18:52 -0400

    Created second test, hardcoded, success

---
+++ src/RomanNumeral.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Minni on 4/5/2016.
+ */
+public class RomanNumeral {
+    public static String arabicToRoman(int arabic){
+        if (arabic == 2){
+            return "II";
+        }
+        return "I";
+    }
+}
---
+++ test/RomanNumeralTest.java
@@ -0,0 +1,14 @@
+import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
+
+/**
+ * Created by Minni on 4/5/2016.
+ */
+public class RomanNumeralTest {
+    @Test
+
+    public void ConvertArabicNumberOneToRomanNumeralOne() {
+        assertEquals("1","I", RomanNumeral.arabicToRoman(1));
+        assertEquals("2", "II", RomanNumeral.arabicToRoman(2));
+    }
+}
