493d2a64945cdbb21bd89724032ee2a8ec0b19ef
minniehajj <mhoq@mapsysinc.com>
2016-04-05 15:35:49 -0400

    Created recursion instead of hardcoded Numerals

--- src/RomanNumeral.java
+++ src/RomanNumeral.java
@@ -2,10 +2,11 @@
  * Created by Minni on 4/5/2016.
  */
 public class RomanNumeral {
-    public static String arabicToRoman(int arabic){
-        if (arabic == 2){
-            return "II";
+    public static String arabicToRoman(int arabicNumber){
+        StringBuilder result = new StringBuilder();
+        for(int i = 0;i < arabicNumber; i++) {
+            result.append("I");
         }
-        return "I";
+        return result.toString();
     }
 }
