ab2b219434ad049106f0b757da0fc36d345e7a58
minniehajj <mhoq@mapsysinc.com>
2016-04-06 15:56:32 -0400

    cleaned up code, deleted unnecessary while loop

--- src/RomanNumeral.java
+++ src/RomanNumeral.java
@@ -2,7 +2,7 @@
  * Created by Minni on 4/5/2016.
  */
 public class RomanNumeral {
-    private static int[]    arabicNumbers  = { 1000, 900,  500, 400,  100, 90,   50,  40,   10,  9,    5,   4,    1   };
+    private static int[]    arabicNumbers  = { 1000, 900, 500, 400, 100, 90, 50,  40,   10,  9, 5, 4, 1 };
     private static String[] romanNumerals = { "M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
     public static String arabicToRoman(int arabicNumber){
@@ -13,11 +13,11 @@ public class RomanNumeral {
         {
             numLeft = attachRomanNumeral(numLeft, arabicNumbers[i], romanNumerals[i], result);
         }
-        while (numLeft >= 1)
+        /*while (numLeft >= 1)
         {
             result.append("I");
             numLeft -= 1;
-        }
+        }*/
         return result.toString();
     }
     private static int attachRomanNumeral (int arabicNumber, int value, String roman, StringBuilder builder) {
