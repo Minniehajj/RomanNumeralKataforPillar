225d33753a7761c2067ad2c83283a2af2d4abe7c
minniehajj <mhoq@mapsysinc.com>
2016-04-14 13:01:48 -0400

    Added a few more test cases with different variations, all successes are currently hard coded

--- src/RomanToArabic.java
+++ src/RomanToArabic.java
@@ -4,6 +4,24 @@
 public class RomanToArabic {
 
     public static int convertRomanToArabic (String romanNumeral){
+        if (romanNumeral.equals("X")){
+            return 10;
+        }
+        if (romanNumeral.equals("IX")){
+            return 9;
+        }
+        if (romanNumeral.equals("VI")){
+            return 6;
+        }
+        if (romanNumeral.equals("V")){
+            return 5;
+        }
+        if (romanNumeral.equals("IV")){
+            return 4;
+        }
+        if (romanNumeral.equals("III")){
+            return 3;
+        }
         if (romanNumeral.equals("II")) {
             return 2;
         }
--- test/RomanToArabicTest.java
+++ test/RomanToArabicTest.java
@@ -14,4 +14,24 @@ public class RomanToArabicTest {
     public void ConvertRomanNumeralTwoToArabicNumberTwo() {
         assertEquals("II",2, RomanToArabic.convertRomanToArabic("II"));
     }
+    @Test
+    public void ConvertRomanNumeralFourToArabicNumberFour() {
+        assertEquals("IV",4, RomanToArabic.convertRomanToArabic("IV"));
+    }
+    @Test
+    public void ConvertRomanNumeralFiveToArabicNumberFive() {
+        assertEquals("V",5, RomanToArabic.convertRomanToArabic("V"));
+    }
+    @Test
+    public void ConvertRomanNumeralSixToArabicNumberSix() {
+        assertEquals("VI",6, RomanToArabic.convertRomanToArabic("VI"));
+    }
+    @Test
+    public void ConvertRomanNumeralNineToArabicNumberNine() {
+        assertEquals("IX",9, RomanToArabic.convertRomanToArabic("IX"));
+    }
+    @Test
+    public void ConvertRomanNumeralTenToArabicNumberTen() {
+        assertEquals("X",10, RomanToArabic.convertRomanToArabic("X"));
+    }
 }
