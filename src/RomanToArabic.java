18c4bebdeeb30348fed2f447219ee3a27c30391f
minniehajj <mhoq@mapsysinc.com>
2016-04-14 12:09:12 -0400

    First test for converting Roman numeral to Arabic Number passed, hardcoded

---
+++ src/RomanToArabic.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Minni on 4/14/2016.
+ */
+public class RomanToArabic {
+
+    public static int convertRomanToArabic (String romanNumeral){
+        return 1;
+    }
+}
---
+++ test/RomanToArabicTest.java
@@ -0,0 +1,13 @@
+import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
+
+/**
+ * Created by Minni on 4/14/2016.
+ */
+
+public class RomanToArabicTest {
+    @Test
+    public void ConvertRomanNumeralOneToArabicNumberOne() {
+        assertEquals("I",1, RomanToArabic.convertRomanToArabic("I"));
+    }
+}
